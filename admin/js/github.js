3ed885055be7ca104b254f265d8b4bd8515afc06
Allanur Achylov <allanurachylov@gmail.com>
2026-03-13 17:38:11 +0700

    Update github.js

--- admin/js/github.js
+++ admin/js/github.js
@@ -1,6 +1,7 @@
 async function uploadFile(path, base64) {
 
 const token = localStorage.getItem("github_token")
+
 const repo = "achylov2/My-First-Github-Site"
 
 const url = `https://api.github.com/repos/${repo}/contents/${path}`
@@ -10,26 +11,23 @@ message: "upload from admin panel",
 content: base64
 }
 
-fetch(url, {
-method: "PUT",
-headers: {
-Authorization: `token ${token}`,
-"Content-Type": "application/json"
+const res = await fetch(url,{
+method:"PUT",
+headers:{
+Authorization:`token ${token}`,
+"Content-Type":"application/json"
 },
-body: JSON.stringify(data)
+body:JSON.stringify(data)
 })
-.then(async res => {
 
 const result = await res.json()
 
 console.log(result)
 
-if(result.content){
-alert("File uploaded successfully 🚀")
+if(res.ok){
+alert("✅ Uploaded!")
 }else{
-alert("Error: " + result.message)
+alert("❌ Error: " + result.message)
 }
-
-})
 
 }
